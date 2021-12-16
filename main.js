5dfd0da8908f5d79fe17a5b9ed43b905dca2307b
Genimarcio <genimarcio.silva@gmail.com>
2021-12-15 21:10:07 -0300

    Versão 15/12/2021

---
+++ main.js
@@ -0,0 +1,6 @@
+function modificarEstilo(corDeFundo, altura) {
+    console.log(corDeFundo)
+
+    document.getElementById('cor_background').style.backgroundColor = corDeFundo
+    document.getElementById('cor_background').style.height = altura
+}
